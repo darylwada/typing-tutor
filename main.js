c83307aaf50e66d92cdf7096ebf15f44c352a1c7
Daryl Wada <daryl.wada@gmail.com>
2018-05-31 11:18:50 -0700

    Defined a function that renders all characters and appends them to the page. #1

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 var phrase = 'grumpy wizards make toxic brew for the evil queen and jack'
 
-var chars = phrase.split('').map( (char, i) => {
-  return { char }
+var chars = phrase.split('').map((char, i)=> {
+  return {char}
 })
 
-function renderChar(char) {
+function renderChar(charObj) {
   var $char = document.createElement('span')
-  $char.textContent = char
+  $char.textContent = charObj.char
   return $char
+}
+
+function renderAllChars(chars) {
+  chars.forEach(charObj => {
+    document.body.appendChild(renderChar(charObj))
+  })
 }
