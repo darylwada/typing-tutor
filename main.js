bed02283836d959ca2d44b27f3113510ae329c55
Daryl Wada <daryl.wada@gmail.com>
2018-05-31 11:13:38 -0700

    Defined a function that renders a single character. #1

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ var phrase = 'grumpy wizards make toxic brew for the evil queen and jack'
 var chars = phrase.split('').map( (char, i) => {
   return { char }
 })
+
+function renderChar(char) {
+  var $char = document.createElement('span')
+  $char.textContent = char
+  return $char
+}
